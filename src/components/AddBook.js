6aaf7ce9de167ee0290eccf3f8f4cecfe027c9d9
Donard97 <donardgolaj@gmail.com>
2021-10-05 16:20:29 +0200

    Create form to add books

---
+++ src/components/AddBook.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-key */
+import React from 'react';
+
+const AddBook = () => (
+  <form>
+    <input type="text" name="Title" placeholder="Title" />
+    <input type="text" name="Author" placeholder="Author" />
+    <input type="button" value="Add" />
+  </form>
+);
+
+export default AddBook;
