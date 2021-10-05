f3576e25a816dbca30cea0462b8d18a31ea2c3bd
Donard97 <donardgolaj@gmail.com>
2021-10-05 16:19:11 +0200

    Create app function to return container

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from './components/Container';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container />
+    </div>
+  );
+}
+
+export default App;
