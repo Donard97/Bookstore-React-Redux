58e6d47dce26b76d4f4580f605c7d8a36aa76365
Donard97 <donardgolaj@gmail.com>
2021-10-06 13:46:06 +0200

    Delete books and add unique id for each books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,28 +1,39 @@
-/* eslint-disable react/jsx-key */
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook, removeBook } from '../redux/books/books';
 import Book from './Book';
+import BookAdd from './AddBook';
 
 const BookList = () => {
-  const books = [
-    {
-      id: 1, name: 'The Power of Now', category: 'Psychology', author: 'Exchart Tolle',
-    },
-    {
-      id: 2, name: 'The Road Less Traveled', category: 'Psychology', author: 'Scott Peck',
-    },
-  ];
+  const myBooks = useSelector((state) => state.booksReducer);
 
+  const dispatch = useDispatch();
+
+  const addNew = (title, author) => {
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+  };
+
+  const deleteBook = (id) => dispatch(removeBook(id));
+
+  if (myBooks.length) {
+    return (
+      <div>
+        <Book books={myBooks} propsToRemoveBooks={deleteBook} />
+        <BookAdd propsToAddBooks={addNew} />
+      </div>
+    );
+  }
   return (
-    <ul>
-      {books.map((book) => (
-        <Book
-          key={book.id}
-          name={book.name}
-          author={book.author}
-        />
-      ))}
-    </ul>
-
+    <div>
+      <h3>Not any book added !</h3>
+      <BookAdd propsToAddBooks={addNew} />
+    </div>
   );
 };
 
