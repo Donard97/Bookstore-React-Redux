27647da981facce42520442cba5b89ce1fbbf5bb
Donard97 <donardgolaj@gmail.com>
2021-10-06 13:51:14 +0200

    Run linters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
