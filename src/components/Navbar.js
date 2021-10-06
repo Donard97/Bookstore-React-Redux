a8a6e12fcc3ce09887ae0c46e9d390858a9c912f
Donard97 <donardgolaj@gmail.com>
2021-10-06 13:40:19 +0200

    Change to links on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,41 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
 const Navbar = () => {
   const links = [
     {
-      id: 2,
+      id: 1,
       path: '/books',
       text: 'Books',
     },
     {
-      id: 3,
+      id: 2,
       path: '/categories',
       text: 'Categories',
     },
   ];
   return (
-    <nav>
-      <h1>Bookstore CMS</h1>
-      <ul className="menu">
-        {links.map((link) => (
-          <li key={link.id}>
-            <NavLink to={link.path} exact>
-              {link.text}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <div className="menu">
+          <h1>Bookstore CMS</h1>
+          <ul>
+            {links.map((link) => (
+              <li key={link.id}>
+                <Link
+                  to={link.path}
+                  className="nav-link"
+                  exact="true"
+                >
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 export default Navbar;
