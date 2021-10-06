0cfdad6d9f130871f8396c40b6d1bf2c2a88976f
Donard97 <donardgolaj@gmail.com>
2021-10-07 00:23:01 +0200

    Add Provider to ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Provider store={store}>
       <App />
-    </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
