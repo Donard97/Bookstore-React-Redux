108200643df23830bb6bef175dc7762783689140
Donard97 <donardgolaj@gmail.com>
2021-10-06 13:39:27 +0200

    Delete container and add Redux store on App file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import Container from './components/Container';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Navbar from './components/Navbar';
+import store from './redux/configureStore';
+import AllBooks from './components/BookList';
+import Categories from './components/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <Container />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <AllBooks />
+          </Route>
+          <Route path="/categories">
+            <Categories />
+          </Route>
+        </Switch>
+      </div>
+    </Provider>
   );
 }
 
