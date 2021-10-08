5f1bb4f3251c033280e364f46f7edbb76ff444b8
Donard97 <donardgolaj@gmail.com>
2021-10-08 11:02:48 +0200

    Add bootstrap classes to app function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import './App.css';
 import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import Categories from './components/Categories';
 
 const App = () => (
   <Router>
-    <Navbar />
-    <Switch>
-      <Route exact path="/">
-        <BookList />
-      </Route>
-      <Route path="/categories">
-        <Categories />
-      </Route>
-    </Switch>
+    <div className="wrapper border shadow mt-5">
+      <Navbar />
+      <Switch>
+        <div className="container">
+          <Route exact path="/">
+            <BookList />
+          </Route>
+          <Route path="/categories">
+            <Categories />
+          </Route>
+        </div>
+      </Switch>
+    </div>
   </Router>
 );
 
