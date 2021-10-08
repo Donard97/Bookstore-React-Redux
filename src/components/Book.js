b79e7e35c52c81e4ffec8e877407bb3c95ac18e2
Donard97 <donardgolaj@gmail.com>
2021-10-08 11:03:50 +0200

    Add bootstrap for buttons and details of books

--- src/components/Book.js
+++ src/components/Book.js
@@ -12,10 +12,34 @@ const Book = ({ book }) => {
 
   return (
     <div>
-      <li>
-        <p>{book.title}</p>
-        <p>{book.category}</p>
-        <button onClick={() => removeBookFromStore(book.item_id)} type="button">Remove</button>
+      <li className="list-group-item d-flex justify-content-between align-items-start p-3 m-3">
+        <div className="ms-2">
+          <small><i>{book.category}</i></small>
+          <div className="fw-bold text-black m-3">
+            <h3>{book.title}</h3>
+          </div>
+          <button type="button" className="btn btn-link ">Comments</button>
+          <button onClick={() => removeBookFromStore(book.item_id)} type="button" className="btn btn-link ">
+            Remove
+          </button>
+          <button type="button" className="btn btn-link ">Edit</button>
+        </div>
+
+        <div className="flexy mt-2">
+          <div className="oval" />
+          <div>
+            <span className="percent">64%</span>
+            <p className="completed">Completed</p>
+          </div>
+        </div>
+        <div className="d-flex row details">
+          <span className="col currentChapter">Current Chapter</span>
+          <span className="chapter currentLesson "> 176 </span>
+          <div className="col">
+            <button type="submit" className="bg-primary update-btn"> Update Progress</button>
+          </div>
+        </div>
+
       </li>
     </div>
   );
